6b93d4d19a39f440e3c9c561156527af40340305
Jorge Jiménez <gexgt90@gmail.com>
2026-02-06 16:21:37 +0100

    Actualizar tecnologías del proyecto Albumory para reflejar las herramientas utilizadas

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -14,7 +14,7 @@ export const projects = [
     title: "ALBUMORY",
     description: "Albumory is an app designed for music lovers who want to organize, discover, and preserve their favorite albums in one place. The platform allows users to create a personalized music library, easily add albums, and keep a clear record of their preferences over time. With an intuitive interface and a user-centric approach, Albumory makes it easy to explore personal collections, sort by artist, genre, or date, and build a music archive that reflects each listener's identity. Ideal for both casual listeners and discerning music lovers, Albumory transforms the way you manage and enjoy music, putting every favorite album at your fingertips.",
     image: ".//images/albumory.jpg",
-    technologies: ["React", "Postman", "REST API"],
+    technologies: ["Javascript", "CSS", "React", "Node.js", "MongoDB", "Google Authentication"],
     github: "https://github.com/GeX90",
     demo: "https://albumory-music.vercel.app/",
     featured: true
